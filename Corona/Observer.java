64f72513bf6418fd731871903eb8142241b1d1fd
gamze184 <52882389+gamze184@users.noreply.github.com>
2020-11-03 23:55:52 +0300

    Create Observer.java

---
+++ Corona/Observer.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author sirak
+ */
+public interface Observer {
+    public void update(String günlüktest, String vaka, String yogunbakim,
+            String entübe, String ölü, String iyilesen, String Tarih,
+            String bugun_test1, String bugun_vaka1, String bugun_vefat1, String bugun_iyilesen1);
+}
