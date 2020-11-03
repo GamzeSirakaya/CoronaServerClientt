455056b45e0c7bc47dc4f673cec1217b4d4104f4
gamze184 <52882389+gamze184@users.noreply.github.com>
2020-11-03 23:56:00 +0300

    Create Subject.java

---
+++ Corona/Subject.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author sirak
+ */
+public interface Subject {
+       public void addObserver(Observer o);  // eklemek için
+    public void removeObserver(Observer o);  // silmek için
+    public void notifyObserver();
+    
+}
